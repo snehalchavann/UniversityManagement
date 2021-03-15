71c825d86ddb12a71154aac75490dd004e7ed591
snehalchavann <78457966+snehalchavann@users.noreply.github.com>
2021-03-15 00:24:21 -0400

    Initial commit

---
+++ src/info5100/university/example/CourseSchedule/CourseLoad.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info5100.university.example.CourseSchedule;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+//import java.util.Map;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class CourseLoad {
+    String semester;
+    ArrayList<SeatAssignment> seatassignments;
+    
+    public CourseLoad(String s){
+        seatassignments = new ArrayList<SeatAssignment>();
+        semester = s;
+    }
+    public SeatAssignment newSeatAssignment(CourseOffer co){
+        
+        Seat seat = co.getEmptySeat(); // seat linked to courseoffer
+        if (seat==null) return null;
+        SeatAssignment sa = seat.newSeatAssignment();
+        seatassignments.add(sa);  //add to students course 
+        return sa;
+    }
+    
+    public void registerStudent(SeatAssignment sa){
+        
+        
+        sa.assignSeatToStudent(this);
+        seatassignments.add(sa);
+    }
+    
+    public ArrayList<String> getListOfCoursesCompleted(HashMap<String, CourseLoad> courseloadlist){
+        ArrayList<String> courses = new ArrayList();
+            for(CourseLoad cl : courseloadlist.values()){
+                for(SeatAssignment sa : cl.seatassignments){
+                           courses.add(sa.seat.getCourseName());
+                }
+        }
+        return courses;
+    } 
+    
+}
